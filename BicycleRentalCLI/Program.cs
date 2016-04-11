a5e0089ef24df2496583136a1354415efc3df886
Evan Lewis <evanlws12@gmail.com>
2016-04-10 20:02:19 -0400

    Initial Commit

---
+++ BicycleRentalCLI/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BicycleRentalCLI
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+    }
+  }
+}
